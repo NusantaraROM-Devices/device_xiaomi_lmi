ea99713862e17666b9cc44ce02701bbc02aee165
nato6613 <normandija1945@gmail.com>
2021-07-09 14:36:56 +0000

    lmi: Add pixel_2016_exclusive.xml
     - This allows us to use unlimited backup in Google Photos infinitely.

--- device.mk
+++ device.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
