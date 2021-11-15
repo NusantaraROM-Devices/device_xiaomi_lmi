83ea441c0601cc84a99712ca3c4485e122d96fd4
ZenkaBestia <z.mail.6600@gmail.com>
2021-11-16 07:19:09 +0800

    lmi: update to Redfin November 2021 fingerprint

--- nad_lmi.mk
+++ nad_lmi.mk
@@ -34,4 +34,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.4.0.RJKCNXM:user/release-keys
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lmi-user 11 RKQ1.200826.002 V12.5.4.0.RJKCNXM release-keys"
