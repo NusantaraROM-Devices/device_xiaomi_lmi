3e85bcc9d1acc89276e0fe7f8c1c93ec2739b428
ZenkaBestia <z.mail.6600@gmail.com>
2022-06-10 06:25:12 +0000

    lmi: update to Raven June 2022 fingerprint

--- nad_lmi.mk
+++ nad_lmi.mk
@@ -37,7 +37,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lmi-user 11 RKQ1.200826.002 V12.5.4.0.RJKCNXM release-keys"
